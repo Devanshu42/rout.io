7b2a1f6c817fd57e06d2e55ea0756cabf7e6da29
anshulchauhan9993 <anshulchauhan9993@gmail.com>
2020-08-24 22:55:03 +0530

    added exception

---
+++ src/main/java/com/gladiator/exceptions/FarmerServiceException.java
@@ -0,0 +1,33 @@
+package com.gladiator.exceptions;
+
+@SuppressWarnings("serial")
+public class FarmerServiceException extends RuntimeException {
+
+	public FarmerServiceException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public FarmerServiceException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FarmerServiceException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FarmerServiceException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FarmerServiceException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
